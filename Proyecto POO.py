fbe242db7b9122207809c6bc6bdc66ebc2847ec2
Percrack1 <rupanelchisquin@gmail.com>
2026-05-16 15:01:17 -0600

    Clases

---
+++ Proyecto POO.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class Gama(ABC):
+    @abstractmethod
+    def tipo_gama(self):
+        pass
+
+class Electrodomésticos(Gama):
+    
+
+class Lavadora(Gama):
+
+
+class Refrigerador(Gama):
+
+
+class Microondas(Gama):
